c4fb8eeaf705f475ea682bd1a45cb3fff7490499
tushar14062019 <tusharsankpal2019@gmail.com>
2025-03-12 15:45:55 +0530

    Update prime_range.py

--- prime_range.py
+++ prime_range.py
@@ -5,3 +5,14 @@ Example:
   For start = 10, end = 30:
   11, 13, 17, 19, 23, 29
 """
+
+First_number = int(input("Enter the Number: "))
+Last_number= int(input("Enter the Number: "))
+
+for i in range(First_number, Last_number):
+    num = 1
+    for j in range(2, i-1):
+        if i%j == 0:
+            num = 0
+    if num == 1:
+        print(i)
