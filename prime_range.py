382d7d4b03aa991ea89e93b7551bd0511ffb6269
ashish-panicker <ashish.s.panicker@proton.me>
2025-03-12 14:51:57 +0530

    single sum and prime range programs

---
+++ prime_range.py
@@ -0,0 +1,7 @@
+"""
+Write a Python program that takes two numbers, start and end, and prints all prime numbers in that range.
+
+Example:
+  For start = 10, end = 30:
+  11, 13, 17, 19, 23, 29
+"""
---
+++ single_sum.py
@@ -0,0 +1,11 @@
+"""
+Write a program that repeatedly adds the digits of a number until the sum is a single digit.
+
+Example
+  Input: 9875
+  Step 1: 9 + 8 + 7 + 5 = 29
+  Step 2: 2 + 9 = 11
+  Step 3: 1 + 1 = 2
+  Output: 2
+
+"""
