ebe783a2c09a3231d8dcfa785f519a5b86ee022c
ashish-panicker <ashish.s.panicker@proton.me>
2025-03-12 14:57:45 +0530

    algorithm for single sum

--- single_sum.py
+++ single_sum.py
@@ -1,6 +1,17 @@
 """
 Write a program that repeatedly adds the digits of a number until the sum is a single digit.
 
+Algorithm
+-----------------------
+1. Start
+2. Accept a number
+3. if the number has only one digit then return the number
+4. Extract the digits and calaculate the sume of these digits
+5. Verify if the sum of digits is a single digit number
+6. If yes print the sum
+7. If no repeat step 4
+8. Stop
+
 Example
   Input: 9875
   Step 1: 9 + 8 + 7 + 5 = 29
